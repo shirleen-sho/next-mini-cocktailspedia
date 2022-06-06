3d5d5e632549adc3f93fe772ba6032eaac8bd42f
shirleen-sho <shirleen.sho@gmail.com>
2022-06-06 20:27:39 +0700

    update a to Link

--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,12 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Navbar() {
     return(
         <div className={styles.navbar}>
             <div className='font-bold text-xl cursor-default' href='/'>Cocktailspedia</div>
             <div>
-                <a href='/'>Home</a>
+                <Link href='/'>Home</Link>
             </div>
         </div>
     )
