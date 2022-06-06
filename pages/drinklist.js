0c1b346ce6ccf131f749a940e3c819245f494cd4
shirleen-sho <shirleen.sho@gmail.com>
2022-06-06 20:25:07 +0700

    update empty tag to div

--- pages/drinklist.js
+++ pages/drinklist.js
@@ -7,10 +7,10 @@ function DrinkPosts(props) {
     const item = props.drink
     return (
         <Link className={styles.card} href={`/drinkdetail/${item.idDrink}`}>
-            <>
+            <div className='cursor-pointer'>
                 <Image src={`${item.strDrinkThumb}`} loader={() => item.strDrinkThumb} alt='Cocktails Image' width={250} height={250}/>
                 <p className='py-4 text-center font-medium text-base'>{item.strDrink}</p>
-            </>
+            </div>
         </Link>
     )
 }
